e4d14d5e52fad8e79ffe0d10eaaa6d113fdf32ca
Andrew Holgate <andrewholgate@yahoo.com>
2015-05-02 16:51:57 +0200

    Removed shim and added es5 shim library for IE < 9

--- app/js/views/survey.js
+++ app/js/views/survey.js
@@ -92,29 +92,6 @@ define( [
             this.nextEnable( stepPrevious );
         },
         saveSurvey: function() {
-
-            // Fix for IE8, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString#Polyfill
-            if ( !Date.prototype.toISOString ) {
-                ( function() {
-                    function pad( number ) {
-                        if ( number < 10 ) {
-                            return "0" + number;
-                        }
-                        return number;
-                    }
-                    Date.prototype.toISOString = function() {
-                      return this.getUTCFullYear() +
-                        "-" + pad( this.getUTCMonth() + 1 ) +
-                        "-" + pad( this.getUTCDate() ) +
-                        "T" + pad( this.getUTCHours() ) +
-                        ":" + pad( this.getUTCMinutes() ) +
-                        ":" + pad( this.getUTCSeconds() ) +
-                        "." + ( this.getUTCMilliseconds() / 1000 ).toFixed( 3 ).slice( 2, 5 ) +
-                        "Z";
-                    };
-                }() );
-            }
-
             var results = this.formResults();
 
             // Add a date to the results.
